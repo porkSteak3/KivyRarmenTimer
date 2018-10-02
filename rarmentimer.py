6008cb40a3a94b096c430a64cabb82f3c28c72e5
porkSteak3 <to.furukawa3@gmail.com>
2018-10-02 18:59:41 +0900

    UnSchedule Add

--- rarmentimer.py
+++ rarmentimer.py
@@ -14,16 +14,18 @@ Builder.load_file("rarmentimer.kv")
 
 class RarmenTimer(App):
     stop_watch_start = False
+    seconds = 180
 
     def __init__(self, **kwargs):
         super(RarmenTimer, self).__init__(**kwargs)
         self.title = 'Rarmen Timer'
         now = datetime.now()
         self.wait_time = datetime(now.year, now.month, now.day, hour=0, minute=0, second=0)
-        self.wait_time = self.wait_time + timedelta(seconds=180)
+        self.wait_time = self.wait_time + timedelta(seconds=self.seconds)
 
     def build(self):
-        Clock.schedule_interval(self.update_time, 1)
+        self.root.ids.time.text = '{}'.format(
+            self.wait_time.time().strftime("%M:%S"))
 
     def update_time(self, seconds):
         if self.stop_watch_start:
@@ -36,9 +38,11 @@ class RarmenTimer(App):
         if self.stop_watch_start:
             self.stop_watch_start = False
             self.root.ids.bt_start_stop.text = 'Start'
+            Clock.unschedule(self.update_time)
         else:
             self.stop_watch_start = True
             self.root.ids.bt_start_stop.text = 'Stop'
+            Clock.schedule_interval(self.update_time, 1)
 
     def reset(self):
 
@@ -52,6 +56,7 @@ class RarmenTimer(App):
 
         self.root.ids.time.text = '{}'.format(
             self.wait_time.time().strftime("%M:%S"))
+
 
 if __name__ == '__main__':
     RarmenTimer().run()
